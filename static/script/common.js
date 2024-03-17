a76c88f2289e0afc454d89c3daf44b23a9276039
Karan <ks.karanshrestha@gmail.com>
2024-03-17 02:01:41 -0700

    initial commit

---
+++ static/script/common.js
@@ -0,0 +1,42 @@
+function RenderHome(){
+debugger;
+$.ajax(
+    {url: "/home",
+    type: 'GET',
+    contentType: "application/json",
+    dataType: 'json',
+     success: function(result){
+     debugger;
+     console.log(result);
+
+    $(".maindiv").html(result);
+  },
+                    error: function (xhr, status, error) {
+                    debugger;
+                    console.log(xhr.responseText);
+                        alert(xhr.responseText);
+                    }
+  });
+}
+
+function RenderInbox(){
+debugger;
+$.ajax(
+    {url: "/inbox",
+    type: 'GET',
+    contentType: "application/json",
+    dataType: 'json',
+     success: function(result){
+     debugger;
+     console.log(result);
+
+    $(".maindiv").html(result);
+  },
+                    error: function (xhr, status, error) {
+                    debugger;
+                    console.log(xhr.responseText);
+                        alert(xhr.responseText);
+                    }
+  });
+}
+
